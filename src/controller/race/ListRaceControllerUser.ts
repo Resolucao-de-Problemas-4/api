d31a41bed7983936dfb1d40fec8ce5ba6478d424
Gustavo Diogo <gustavodiogo.aluno@unipampa.edu.br>
2021-12-07 15:49:19 -0300

    feat: lista corridas do usuario

---
+++ src/controller/race/ListRaceControllerUser.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from ".prisma/client";
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+const prisma = new PrismaClient();
+
+interface TokenPayload {
+  id: string;
+  iat: number;
+  exp: number;
+}
+
+class ListRaceControllerUser {
+  async execute(request: Request, response: Response): Promise<Response> {
+    try {
+      const { token } = request.body;
+
+      const data = jwt.verify(token, "secretrp");
+      const { id } = data as TokenPayload;
+
+      const races = await prisma.race.findMany({
+        where: {
+          idCliente: Number(id)
+        }
+      });
+      
+      return response.status(200).json({races});
+    } catch (err) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+  }
+}
+export { ListRaceControllerUser };
--- src/routes/race.routes.ts
+++ src/routes/race.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
+import { ListDriverController, ListRaceControllerDriver, ListRaceControllerUser } from '../controller'
 import { CreateRaceController } from '../controller/race/CreateRaceController'
 
 const create = new CreateRaceController()
-
+const listD = new ListRaceControllerDriver()
+const listU = new ListRaceControllerUser()
 
 const raceRoutes = (router: Router): void => {
   router.post('/api/race', create.execute.bind(CreateRaceController))
-  
+  router.post('/api/race-d-list', listD.execute.bind(ListRaceControllerDriver))
+  router.post('/api/race-u-list', listU.execute.bind(ListRaceControllerUser) )
 }
 
 export { raceRoutes }
