9fc686692a62662164d306569e6e1e888b82dfb8
Gustavo Diogo <67334321+Gustavo-Diogo@users.noreply.github.com>
2022-02-17 13:29:23 -0300

    feat: finishRace also deleting Localization.

--- src/controller/race/FinishRaceController.ts
+++ src/controller/race/FinishRaceController.ts
@@ -15,46 +15,60 @@ class FinishRaceController {
     try {
       const { token, corridaID } = request.body;
       const data = jwt.verify(token, env.SECRET_TOKEN);
-      
-      if(token === null || corridaID === null){
+
+      if (token === null || corridaID === null) {
         throw new Error('ERRO')
       }
 
       const { id } = data as TokenPayload;
-      
+
       const date = new Date();
       const hour = date.getHours() + ":" + date.getMinutes();
 
       const race = await prisma.race.findFirst({
-        where:{
+        where: {
           id: corridaID
         }
       })
 
-      if(!race){
+      if (!race) {
         throw new Error("Essa corrida não existe...")
       }
 
-      if(race.corridaCancelada === true){
-        throw new Error ("Corrida cancelada.")
+      if (race.corridaCancelada === true) {
+        throw new Error("Corrida cancelada.")
       }
 
-      if(race.corridaAceita === false){
-        throw new Error ("Corrida não foi nem aceita ainda...")
+      if (race.corridaAceita === false) {
+        throw new Error("Corrida não foi nem aceita ainda...")
 
+      }
+
+      const driver = await prisma.driver.findUnique({
+        where:{
+          id
+        }
+      })
+
+      if(driver.id !== race.idDriver){
+        throw new Error('Token and DriverID from Race are not the same.')
       }
 
       await prisma.race.update({
         where: {
           id: corridaID
-        }, data:{
-          horaFinal:hour,
-          viagemConcluida:true
+        }, data: {
+          horaFinal: hour,
+          viagemConcluida: true
         }
       });
 
+      await prisma.localization.delete({
+        where: { driverID: id }
+      })
 
-      
+
+
       return response.status(200).json("Concluído!");
     } catch (err) {
       return response.status(400).json({
